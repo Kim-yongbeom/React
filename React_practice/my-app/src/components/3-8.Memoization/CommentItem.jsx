da768535376b386e4cfa333b47df92e973c2f27b
김용범 <subum12345@gmail.com>
2022-04-22 14:30:36 +0900

    Memoization

--- React_practice/my-app/src/components/3-8.Memoization/CommentItem.jsx
+++ React_practice/my-app/src/components/3-8.Memoization/CommentItem.jsx
@@ -1,15 +1,50 @@
-import React, { memo } from 'react'
+import React, { Profiler, memo, useState, useMemo } from 'react'
 import "./CommentItem.css"
 
-function CommentItem({title, content, likes}) {
+function CommentItem({title, content, likes, onClick}) {
+  const [clickCount, setClickCount] = useState(0)
+  function onRenderCallback(
+    id, // 방금 커밋된 Profiler 트리의 "id"
+    phase, // "mount" 트리가 방금 마운트된 경우
+    actualDuration, // 커밋된 업데이트를 렌더링하는데 걸린 시간
+    baseDuration, // 메모이제이션 없이 하위 트리 전체를 렌더링하는데 걸리는 예상시간
+    startTime, // React가 언제 해당 업데이트를 렌더링하기 시작했는지
+    commitTime, // React가 해당 업데이트를 언제 커밋햇는지
+    interactions // 이 업데이트에 해당하는 상호작용들의 집합
+  ){
+    // 렌더링 타이밍을 집합하거나 로그
+    console.log(`actualDuration(${title}: ${actualDuration})`)
+  }
+
+  const handleClick= () => {
+    onClick()
+    setClickCount(prev => prev + 1)
+    alert(`${title}를 ${clickCount+1}만큼 누름`)
+  }
+
+  // useMemo는 리턴한 값을 넣어줌(rate에 넣음)
+  const rate = useMemo(() => {
+    console.log("rate check")
+    return likes > 10 ? 'Good' : 'Bad';
+  },[likes])
+
   return (
-    <div className="CommentItem">
-      <span>{title}</span>
-      <br/>
-      <span>{content}</span>
-      <br/>
-      <span>{likes}</span>
-    </div>
+    // Profiler는 React 애플리케이션이 렌더링하는 빈도와 렌더링 “비용”을 측정합니다. 
+    // Profiler의 목적은 메모이제이션 같은 
+    // 성능 최적화 방법을 활용할 수 있는 애플리케이션의 느린 부분들을 식별해내는 것입니다.
+    <Profiler id="CommentItem" onRender={onRenderCallback}>
+      <div className="CommentItem" onClick={handleClick}>
+        <span>{title}</span>
+        <br/>
+        <span>{content}</span>
+        <br/>
+        <span>{likes}</span>
+        <br/>
+        <span>{rate}</span>
+        <br/>
+        <span>{clickCount}</span>
+      </div>
+    </Profiler>
   )
 }
 
--- React_practice/my-app/src/components/3-8.Memoization/Comments.jsx
+++ React_practice/my-app/src/components/3-8.Memoization/Comments.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import CommentItem from './CommentItem'
 
 export default function Comments({commentList}) {
+  // Comments 자체가 리랜더링 되기때문에 막아주기 위해 useCallback사용
+
+  const handleClick = useCallback(() => {
+    console.log('눌림')
+  },[])
   return (
     <div>
       {commentList.map(comment => <CommentItem
@@ -9,6 +14,7 @@ export default function Comments({commentList}) {
         title= {comment.title}
         content={comment.content}
         likes={comment.likes}
+        onClick={handleClick}
       />)}
     </div>
   )
