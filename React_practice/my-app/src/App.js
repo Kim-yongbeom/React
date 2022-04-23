9c2d8bb2b9fd001eefb758ccf00b5851434e6b25
김용범 <subum12345@gmail.com>
2022-04-23 20:27:44 +0900

    Context

--- React_practice/my-app/src/App.js
+++ React_practice/my-app/src/App.js
@@ -1,8 +1,9 @@
 // import logo from './logo.svg';
 import './App.css';
+import Example from './components/3-10.Context/Example'
 // import Button from './components/3-7.HOC/Button';
 // import Input from './components/3-7.HOC/Input';
-import Memo from './components/3-8.Memoization/Memo';
+// import Memo from './components/3-8.Memoization/Memo';
 // import ThankyouDialog from './components/3-6.Composition2/ThankyouDialog';
 // import Dialog from './components/3-6.Composition2/Dialog';
 // import WelcomeDialog from './components/3-5.Composition/WelcomeDialog';
@@ -21,7 +22,8 @@ import Memo from './components/3-8.Memoization/Memo';
 function App() {
   return (
     <div className="App">
-      <Memo/>
+      <Example/>
+      {/* <Memo/> */}
       {/* <Input/> */}
       {/* <Button/> */}
       {/* <ThankyouDialog/> */}
