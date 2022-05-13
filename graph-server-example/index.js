7a255d21071b14a0254ebf1b2153da2e72220a31
김용범 <subum12345@gmail.com>
2022-05-13 13:05:18 +0900

    GraphQL

---
+++ graph-server-example/index.js
@@ -0,0 +1,47 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+// npm install nodemon을 설치해 코드 변경시 바로 적용되게 해줌
+// 실행은 npm start
+// package.json에서 start 설정해줘야함
+
+const books = [
+  {
+    title: "The Awake22",
+    author: "Kate",
+  },
+  {
+    title: "City",
+    author: "Paul",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+  console.log(`server ${url}`);
+});
