daf49b86b4382863cc01cedc1b59b4bbcee8a779
김용범 <subum12345@gmail.com>
2022-06-03 12:52:25 +0900

    Next.js

---
+++ Next.js/next-app/pages/launches.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+export default function launches() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchLaunches = async () => {
+      const res = await fetch("https://api.spacexdata.com/v3/launches");
+      const data = await res.json();
+
+      setData(data);
+    };
+    fetchLaunches();
+  }, []);
+
+  if (data == null) {
+    return null;
+  }
+
+  return (
+    <div>
+      <ol>
+        {data.map((launch, index) => (
+          <li key={index}>{launch.mission_name}</li>
+        ))}
+      </ol>
+    </div>
+  );
+}
