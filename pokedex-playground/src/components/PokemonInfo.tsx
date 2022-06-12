6ac89622b305f764bd16605fcbe5084a709749e7
김용범 <subum12345@gmail.com>
2022-06-12 19:15:40 +0900

    포켓몬 도감

--- pokedex-playground/src/components/PokemonInfo.tsx
+++ pokedex-playground/src/components/PokemonInfo.tsx
@@ -3,27 +3,82 @@ import styled from "@emotion/styled/macro";
 import { Color, Type } from "../types";
 import { mapColorToHex, mapTypeToHex } from "../utils";
 
-const Base = styled.div``;
+const Base = styled.div<{ color?: string }>`
+  display: flex;
+  flex-direction: column;
+  background: ${({ color }) => color};
+  padding: 20px;
+  border-bottom-left-radius: 20%;
+  border-bottom-right-radius: 20%;
+`;
 
-const ThumbnailImageWrapper = styled.div``;
+const ThumbnailImageWrapper = styled.div`
+  width: 160px;
+  margin-inline: auto;
+  margin-block: 24px;
+`;
 
-const ThumbnailImage = styled.img``;
+const ThumbnailImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`;
 
-const InfoWrapper = styled.div``;
+const InfoWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
 
-const Name = styled.div``;
+const Name = styled.div`
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+  text-transform: capitalize;
+`;
 
-const Index = styled.div``;
+const Index = styled.div`
+  color: #fff;
+  font-size: 36px;
+  font-weight: bold;
+  opacity: 0.75;
+`;
 
-const TypeWrapper = styled.div``;
+const TypeWrapper = styled.div<{ color?: string }>`
+  background-color: ${({ color }) => color};
+  padding: 4px;
+  border-border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-const TypeList = styled.div``;
+const TypeList = styled.div`
+  display: flex;
+  margin-top: 8px;
+  ${TypeWrapper} + ${TypeWrapper} {
+    margin-left: 8px;
+  }
+`;
 
-const TypeInfo = styled.img``;
+const TypeInfo = styled.img`
+  height: 12px;
+`;
 
-const ImageWrapper = styled.div``;
+const ImageWrapper = styled.div`
+  position: absolute;
+  width: 288px;
+  height: 288px;
+  left: -96px;
+  top: -96px;
+  opacity: 0.75;
+`;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`;
 
 interface Props {
   id: string;
--- pokedex-playground/src/pages/DetailPage.tsx
+++ pokedex-playground/src/pages/DetailPage.tsx
@@ -1,15 +1,31 @@
 // 포켓몬 아이디를 파라미터로 전달받아 상세정보 표시
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import Tabs from "../components/Tabs";
 
 type Params = {
   id: string;
 };
 
+type Tab = "about" | "stats" | "evolution";
+
 const DetailPage: React.FC = () => {
   const { id } = useParams<Params>();
+  const [selectedTab, setSelectedTab] = useState<Tab>("about");
 
-  return <div>DetailPage id: {id}</div>;
+  const handleClick = (tab: Tab) => {
+    setSelectedTab(tab);
+  };
+
+  return (
+    <div>
+      <Tabs
+        tab={selectedTab}
+        onClick={handleClick}
+        color={{ name: "red", url: "" }}
+      />
+    </div>
+  );
 };
 
 export default DetailPage;
