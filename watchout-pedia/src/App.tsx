8d401d227a783bf60536ef37dda605bb19519f76
김용범 <subum12345@gmail.com>
2022-06-15 20:03:39 +0900

    왓챠피디아 클론 코딩

---
+++ watchout-pedia/src/App.tsx
@@ -0,0 +1,24 @@
+// npm i react-router-dom
+// npm i @types/react-router-dom -D
+
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainPage from "./Pages/MainPage";
+import TvPage from "./Pages/TvPage";
+import MovieDetail from "./Pages/MovieDetail";
+import TvDetail from "./Pages/TvDetail";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/tv" element={<TvPage />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/tv/:id" element={<TvDetail />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
