a7a3560ac9ebdc247fb9c3f897556512d049b995
김용범 <subum12345@gmail.com>
2022-06-23 19:46:38 +0900

    왓챠피디아 클론코딩 - 진짜 코드

---
+++ watchout-pedia_real/src/App.tsx
@@ -0,0 +1,41 @@
+// npm i react-router-dom
+// npm i @types/react-router-dom -D
+// npm i @emotion/react @emotion/styled
+// npm i axios
+// npm i react-slick
+// npm i @types/react-slick -D
+// npm i react-query
+// npm install --save dotenv
+
+import React from "react";
+import styled from "@emotion/styled";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import MainPage from "./pages/MainPage";
+import MovieDetail from "./pages/MovieDetail";
+import TvPage from "./pages/TvPage";
+import TvDetail from "./pages/TvDetail";
+
+import LoginModal from "./features/app/LoginModal";
+import SignupModal from "./features/app/SignupModal";
+
+const Base = styled.div``;
+
+function App() {
+  return (
+    <Base>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/tv" component={TvPage} />
+          <Route exact path="/movie/:id" component={MovieDetail} />
+          <Route exact path="/tv/:id" component={TvDetail} />
+        </Switch>
+      </Router>
+      <LoginModal />
+      <SignupModal />
+    </Base>
+  );
+}
+
+export default App;
