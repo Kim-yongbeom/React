3de650915e6950f172ad47648334c882a79e1647
김용범 <subum12345@gmail.com>
2022-06-30 17:11:34 +0900

    api호출 연습

---
+++ practice/src/App.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [name, setName] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setName(data.results);
+      });
+  }, []);
+  console.log(name);
+
+  return (
+    <>
+      {name.map((i) => (
+        <ul>
+          <li>
+            name: {i.name}
+            <br />
+            url: {i.url}
+          </li>
+        </ul>
+      ))}
+    </>
+  );
+}
+
+export default App;
