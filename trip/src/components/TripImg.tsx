025c1228983705a57236aeb4d51e65510403b9c5
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2022-07-05 23:50:22 +0900

    트리플 애니메이션

--- trip/src/components/TripImg.tsx
+++ trip/src/components/TripImg.tsx
@@ -7,7 +7,7 @@ import triple2x from "../imgs/triple2x.png";
 const FadeIn = keyframes`
   0% {
       opacity: 0;
-      // x축, y축, z축 => y축 10%만큼 이동
+      // x축, y축, z축 => y축 5%만큼 이동
       transform: translate3d(0, 5%, 0);
   }
   to {
