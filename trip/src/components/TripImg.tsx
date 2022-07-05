2a04c4ad041546db54acc2b240fcd69270a3ec65
김용범 <subum12345@gmail.com>
2022-07-05 10:55:36 +0900

    트리플 레이아웃

--- trip/src/components/TripImg.tsx
+++ trip/src/components/TripImg.tsx
@@ -2,20 +2,21 @@ import React from "react";
 import styled from "@emotion/styled/macro";
 import triple2x from "../imgs/triple2x.png";
 
-const Base = styled.div<{ url?: string }>`
-  position: absolute;
+const Base = styled.div`
+  position: relative;
   top: 9.375rem;
-  width: 25rem;
-  height: 21.125rem;
-  background-size: 25rem 21.125rem;
-  padding-top: 17.5rem;
-  box-sizing: border-box;
-  ${({ url }) => url && `background-image: url(${url})`};
-  background-repeat: no-repeat;
   font-size: 0.9375rem;
 `;
 
-const TextWrapper = styled.div`
+const TextWrapper = styled.div<{ url?: string }>`
+  position: absolute;
+  width: 25rem;
+  height: 21.125rem;
+  padding-top: 17.5rem;
+  background-size: 25rem 21.125rem;
+  box-sizing: border-box;
+  ${({ url }) => url && `background-image: url(${url})`};
+  background-repeat: no-repeat;
   text-align: center;
   font-family: sans-serif;
   color: rgba(58, 58, 58, 0.7);
@@ -25,8 +26,8 @@ const Text = styled.div``;
 
 const TripImg: React.FC = () => {
   return (
-    <Base url={triple2x}>
-      <TextWrapper>
+    <Base>
+      <TextWrapper url={triple2x}>
         <Text>2021년 12월 기준</Text>
       </TextWrapper>
     </Base>
