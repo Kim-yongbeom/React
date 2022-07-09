e984b539bb163c0c5398340767f53a28b241217f
김용범 <subum12345@gmail.com>
2022-07-09 14:07:49 +0900

    my-book

--- my-books/src/App.tsx
+++ my-books/src/App.tsx
@@ -1,10 +1,18 @@
 // npm i react-router-dom
 // npm i --save-dev @types/react-router-dom
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
