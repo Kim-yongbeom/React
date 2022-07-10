51c6cf4239a0a52098e50c77ff1b67599dfbad4e
김용범 <subum12345@gmail.com>
2022-07-10 20:58:09 +0900

    my-books 라우팅 설정

--- my-books/src/App.tsx
+++ my-books/src/App.tsx
@@ -1,22 +1,36 @@
 // npm i react-router-dom
 // npm i --save-dev @types/react-router-dom
+
+// 런타임에 문제가 있을때 에러페이지로 이동 시킬수 있다.
+// npm i react-error-boundary
+
 import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
+import Add from "./pages/Add";
+import Detail from "./pages/Detail";
+import Edit from "./pages/Edit";
+import Error from "./pages/Error";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/signin" element={<Signin />} /> */}
-        {/* <Route path="/add" element={<Add />} /> */}
-        {/* <Route path="/book/:id" element={<Detail />} /> */}
-        {/* <Route path="/edit/:id" element={<Edit />} /> */}
-        {/* <Route element={NotFound} /> */}
-      </Routes>
-    </BrowserRouter>
+    // npm i react-error-boundary를 설치해 ErrorBoundary사용 Error페이지로 이동하게함
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/book/:id" element={<Detail />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
---
+++ my-books/src/pages/Add.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Add: React.FC = () => {
+  return (
+    <div>
+      <h1>Add</h1>
+    </div>
+  );
+};
+
+export default Add;
