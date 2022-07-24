3379d115376e95a52968bf43f4f5412e3899e10c
김용범 <subum12345@gmail.com>
2022-07-24 23:48:24 +0900

    redux 연습

--- redux/src/App.tsx
+++ redux/src/App.tsx
@@ -1,35 +1,40 @@
 // npm install @types/react-redux
 import React from "react";
-import "./App.css";
 import { legacy_createStore as createStore } from "redux";
 import styled from "@emotion/styled/macro";
 
-const Base = styled.div<{ state: any }>`
+const Base = styled.div<{ state: string }>`
+  border: 10px solid gray;
   background-color: ${({ state }) => state};
 `;
 
 // store에서의 state 초기값 설정
 function reducer(state: any, action: any) {
+  console.log(state, action);
   if (state === undefined) {
-    return { color: "red" };
+    return { color: "yellow" };
   }
+  let newState;
+  if (action.type === "CHANGE_COLOR") {
+    newState = Object.assign({}, state, { color: "red" });
+  }
+  return newState;
 }
 
 // store 생성
 const store = createStore(reducer);
 
-let state: any = undefined;
-
 function App() {
-  const dom = () => {
-    console.log(state);
-    state = store.getState();
+  const state: any = store.getState();
+
+  const Color = () => {
+    store.dispatch({ type: "CHANGE_COLOR", color: "red" });
   };
 
   return (
-    <Base state={state}>
+    <Base state={state.color}>
       <h1>red</h1>
-      <input type="button" value="fire" onClick={dom} />
+      <input type="button" value="fire" onClick={Color} />
     </Base>
   );
 }
