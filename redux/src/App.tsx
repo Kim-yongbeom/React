8cd1434a39c37e60d10fa76a4ca3d27371c8ea7d
김용범 <subum12345@gmail.com>
2022-07-25 22:11:59 +0900

    react-redux 기본 설정

--- redux/src/App.tsx
+++ redux/src/App.tsx
@@ -1,44 +1,8 @@
 // npm install @types/react-redux
 import React from "react";
-import { legacy_createStore as createStore } from "redux";
-import styled from "@emotion/styled/macro";
-
-const Base = styled.div<{ state: string }>`
-  border: 10px solid gray;
-  background-color: ${({ state }) => state};
-`;
-
-// store에서의 state 초기값 설정
-function reducer(state: any, action: any) {
-  console.log(state, action);
-  if (state === undefined) {
-    return { color: "yellow" };
-  }
-  let newState;
-  if (action.type === "CHANGE_COLOR") {
-    newState = Object.assign({}, state, { color: action.color });
-  }
-  return newState;
-}
-
-// store 생성
-const store = createStore(reducer);
 
 function App() {
-  const state: any = store.getState();
-
-  const Color = () => {
-    store.dispatch({ type: "CHANGE_COLOR", color: "red" });
-  };
-
-  store.subscribe(Color);
-
-  return (
-    <Base state={state.color}>
-      <h1>red</h1>
-      <input type="button" value="fire" onClick={Color} />
-    </Base>
-  );
+  return <div>redux</div>;
 }
 
 export default App;
