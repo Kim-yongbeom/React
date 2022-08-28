b1d2a0f185f9e47f799370676af7a1485497213e
김용범 <subum12345@gmail.com>
2022-08-28 12:02:18 +0900

    react-router-dom

--- mall/src/App.js
+++ mall/src/App.js
@@ -1,7 +1,17 @@
 // npm i @emotion/styled @emotion/react
+// npm i react-router-dom@5
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
