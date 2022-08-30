8682641c1b8d0ac209751774c51143dccdb3438b
김용범 <subum12345@gmail.com>
2022-08-30 22:27:48 +0900

    product

--- mall/src/pages/Home.js
+++ mall/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "../components/Nav/Nav";
+import Product from "../components/Products/Product";
 import Sliders from "../components/Sliders";
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
       <Nav />
       <div>Home</div>
       <Sliders />
+      <Product />
     </>
   );
 }
